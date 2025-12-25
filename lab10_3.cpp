bd598d1c7ec6b492bb9d6b1d90e5ca0f0efcfabc
Mix <pison.mick1@gmail.com>
2025-12-25 17:14:59 +0700

    lab10_3.cpp

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,24 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    int count = 0;
+    float sum = 0;
+    float sst = 0;
+    string textline;
+    ifstream source("score.txt");
+    while(getline(source,textline)){
+        sum += atof(textline.c_str());
+        sst += (pow(atof(textline.c_str()),2));
+        count++;
+    }
+    cout << "Number of data = " << count << "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << sum/count << "\n";
+    cout << "Standard deviation = " << sqrt((((sst)/count))-(pow((sum/count),2)));
+}
